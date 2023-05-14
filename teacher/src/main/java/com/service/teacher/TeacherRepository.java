742a5b5c4b6b47a21e6e5bfe243c11d03ddbb1ee
dhanushka365 <technohub10000@gmail.com>
2023-05-14 15:21:10 +0530

    teacher repository updated

--- teacher/src/main/java/com/service/teacher/TeacherRepository.java
+++ teacher/src/main/java/com/service/teacher/TeacherRepository.java
@@ -3,4 +3,6 @@ package com.service.teacher;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TeacherRepository extends JpaRepository<Teacher, Integer > {
+
+    Teacher findByEmail(String email);
 }
